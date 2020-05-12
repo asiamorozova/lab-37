ec7555e4a9a11d41d17293e368820d9a8b59bb77
Anastasia Morozova <morozova.asia@gmail.com>
2020-05-12 16:55:03 -0700

    reducer errors fixed

--- src/reducers/actionReducer.test.js
+++ src/reducers/actionReducer.test.js
@@ -2,38 +2,27 @@ import { drinkCoffee, eatSnack, takeNap, study } from '../actions/Actions';
 import reducer from './actionReducer';
 
 describe('it handles reducer functionality', () => {
-    it('handles drink coffee' , () => {
-        const state = {
-            coffee: 0,
-            snack: 0,
-            nap: 0,
-            study: 0
-        };
-        const action = drinkCoffee();
-        const (newState).toEqual({
-            coffee: 1,
-            snack: 0,
-            nap: 0,
-            study: 0
-        });
+  it('handles drink coffee', () => {
+    const state = {
+      coffee: 0,
+      snack: 0,
+      nap: 0,
+      study: 0
+    };
+
+    const action = drinkCoffee();
+    const newState = reducer(state, action);
+
+    expect (newState).toEqual({
+      coffee: 1,
+      snack: 0,
+      nap: 0,
+      study: 0
     });
+  });
 
 
-it('handles drink coffee' , () => {
-        const state = {
-            coffee: 0,
-            snack: 0,
-            nap: 0,
-            study: 0
-        };
-        const action = drinkCoffee();
-        const (newState).toEqual({
-            coffee: 1,
-            snack: 0,
-            nap: 0,
-            study: 0
-        });
-    });
 
 
-})
+
+});
