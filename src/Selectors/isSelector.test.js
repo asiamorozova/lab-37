e2bfb1fa0d5b7bc9a4ccf467cc694936a31500c9
Anastasia Morozova <morozova.asia@gmail.com>
2020-05-12 18:46:10 -0700

    isTired test is passing

--- src/Selectors/isSelector.test.js
+++ src/Selectors/isSelector.test.js
@@ -3,10 +3,10 @@ import { isTired, isHyper, isEducated, isHungry } from './isSelector';
 describe('selector test', () => {
   it('isTired if coffee and naps are less then 1', () => {
     const state = {
-      coffee: 0,
-      snack: 1,
-      nap: 0,
-      study: 1
+      coffees: 0,
+      snacks: 1,
+      naps: 0,
+      studies: 1
 
     };
     const tired = isTired(state);
